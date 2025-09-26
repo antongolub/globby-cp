8f48d06dce63a012397fb5e92367de13884cf2ea
Anton Golub <antongolub@antongolub.com>
2025-09-26 21:01:35 +0300

    chore: minor code imprs

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,23 +1,22 @@
+import fs from 'node:fs/promises'
 import path from 'node:path'
 import process from 'node:process'
-import { cp as fsCopy, lstat as fsStat, mkdir as fsMkdir } from 'node:fs/promises'
 import { globby } from 'globby'
 
 const SPECIALS = '*{}[]?!'.split('')
 const isPattern = (src) => SPECIALS.some((c) => src.includes(c))
-const cp = async (src, dest, debug) => {
-  debug('copy', 'from=', src, 'to=', dest)
-  return fsCopy(src, dest, { recursive: true })
-}
-
 const isDir = async (src) => {
   if (isPattern(src)) return false
   if (src.endsWith('/')) return false
   try {
-    return (await fsStat(src)).isDirectory()
+    return (await fs.lstat(src)).isDirectory()
   } catch {
     return false
   }
+}
+const cp = async (src, dest, debug) => {
+  debug('copy', 'from=', src, 'to=', dest)
+  return fs.cp(src, dest, { recursive: true })
 }
 
 export const copy = async ({
@@ -32,17 +31,23 @@ export const copy = async ({
   if (!from || !to) throw new Error('Both `from` and `to` arguments are required')
   if (isPattern(to)) throw new Error('`to` must not be a glob pattern')
 
-  const _to = path.resolve(baseTo, to)
-  if (to.endsWith('/')) await fsMkdir(_to, { recursive: true })
-
   const { patterns, dirs } = await parseSources(from, baseFrom)
+  const _to = path.resolve(baseTo, to)
+
+  if (to.endsWith('/')) await fs.mkdir(_to, { recursive: true })
 
   if (dirs.length === 0 && patterns.length === 1 && !isPattern(patterns[0]) || dirs.length === 1 && patterns.length === 0) {
     const f = dirs[0] || path.resolve(baseFrom, patterns[0])
     if (!!dirs[0] === await isDir(_to)) return cp(f, _to, debug)
   }
 
-  await globby(patterns, { dot: true, ...opts, cwd: baseFrom, absolute: true, ignoreFiles }).then((files) =>
+  await globby(patterns, {
+    dot: true,
+    ...opts,
+    cwd: baseFrom,
+    absolute: true,
+    ignoreFiles
+  }).then((files) =>
     Promise.all([
       ...files.map((file) => cp(
         file,
